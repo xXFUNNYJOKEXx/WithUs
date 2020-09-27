39abfddfac45b98fd9a74e3446e867e9fa61ef6f
xXFUNNYJOKEXx <70741449+xXFUNNYJOKEXx@users.noreply.github.com>
2020-09-27 18:01:42 +1000

    ServerCodeAdded

---
+++ White blood cells and Corona/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.Diagnostics;
+using static System.Diagnostics.Debug;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+
+    public static NetworkManager instance;
+
+    void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master server");
+    }
+
+    public void CreateRoom(string roomName)
+    {
+        PhotonNetwork.CreateRoom(roomName);
+    }
+
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+}
